194f6ea88c5f469ceac74c7f6c1e799aefd09069
999benson <bensonwu509@gmail.com>
2022-04-04 09:13:50 +0800

    add dotdev

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     meter: {
       url: "https://rpctest.meter.io",
-      accounts: ["6fecf28b6582882d129a0e081f32f88a706316efd2786a7b626d8207c04b8e01"]
+      accounts: [PRIVATE_KEY]
     }
   },
   solidity: {
